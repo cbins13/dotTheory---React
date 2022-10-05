568d745dc3fa79866ddc9d2075f64a5f9eb11308
Christian Binay-an <christianbinayan3@gmail.com>
2022-10-05 19:54:54 +0800

    RCT-140: Upload React Basics Code Challenge Files

---
+++ RCT-101/React_Basics/Thinking_in_Components/scoreboard/app.js
@@ -0,0 +1,40 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Scoreborad</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+};
+
+const Counter = () => {
+  return(
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+}
+
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">Guil</span>
+
+      <Counter />
+    </div>
+  );
+};
+
+
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+      <Player />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
