48dd86515e833996266fef437ae0720f89f05b00
Christian Binay-an <christianbinayan3@gmail.com>
2022-10-21 20:07:05 +0800

    RCT-144: Update Routes and implement active NavLinks function

--- RCT-202/scoreboard_final/src/components/App.js
+++ RCT-202/scoreboard_final/src/components/App.js
@@ -1,21 +1,31 @@
 import React from "react";
 import Nav from "./Nav";
 import Header from "./Header";
-import { BrowserRouter, Route } from "react-router-dom";
+import NotFound from "./NotFound";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PlayerList from "./PlayerList";
 import AddPlayerForm from "./AddPlayerForm";
+import FeaturedPlayer from "./FeaturedPlayer";
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
-    <Route path="/" component={Nav} />
-    <div className="scoreboard">
-      <Route path="/header" component={Header} /> 
-      <Route path="/playerlist" component={PlayerList} /> 
-      <Route path="/addplayer" component={AddPlayerForm} /> 
+      <Route path="/" component={Nav} />
+      <div className="scoreboard">
+        <Switch>
+          <Route exact path="/">
+            <Header />
+            <PlayerList />
+            <AddPlayerForm />
+          </Route>
+          <Route path="/header" component={Header} />
+          <Route exact path="/playerlist" component={PlayerList} />
+          <Route path="/playerlist/:name/:score?" component={FeaturedPlayer} />
+          <Route path="/addplayer" component={AddPlayerForm} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
     </div>
-    </div>
-    
   </BrowserRouter>
 );
 
--- RCT-202/scoreboard_final/src/components/Nav.js
+++ RCT-202/scoreboard_final/src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => (
   <nav>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink exact to="/">Home</NavLink></li>
       <li><NavLink to="/header" >Header</NavLink></li>
       <li><NavLink to="/playerlist">PlayerList</NavLink></li>
       <li><NavLink to="/addplayer">AddPlayer</NavLink></li>
