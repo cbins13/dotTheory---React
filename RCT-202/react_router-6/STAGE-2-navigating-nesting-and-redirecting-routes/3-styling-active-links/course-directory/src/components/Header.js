c65f37d4f1e0fd4c18120660441bcac5a0d4d46a
Christian Binay-an <christianbinayan3@gmail.com>
2022-11-22 18:31:25 +0800

    RCT-143: Upload Stage 1-3 Solution files

---
+++ RCT-202/react_router-6/STAGE-2-navigating-nesting-and-redirecting-routes/3-styling-active-links/course-directory/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <span className="icn-logo"><i className="material-icons">code</i></span>
+    <ul className="main-nav">
+      <li><NavLink to="/" end style={({isActive}) => isActive ? {background: 'tomato'} : undefined}>Home</NavLink></li>
+      <li><NavLink to="about" className={({isActive})=> isActive ? "custom-class-name" : undefined}>About</NavLink></li>
+      <li><NavLink to="teachers">Teachers</NavLink></li>
+      <li><NavLink to="courses">Courses</NavLink></li>
+    </ul>    
+  </header>
+);
+
+export default Header;
